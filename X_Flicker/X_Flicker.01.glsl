2e260a103a4e99752e80273d2a31161c891bd94c
noesclavo <spam@noesclavo.com>
2014-06-03 11:47:55 -0700

    Made much mo simpler.

--- X_Flicker/X_Flicker.01.glsl
+++ X_Flicker/X_Flicker.01.glsl
@@ -1,12 +1,29 @@
 #version 120
 
 uniform float adsk_result_w, adsk_result_h;
-uniform sampler2D Front;
+vec2 res = vec2(adsk_result_w, adsk_result_h);
+
+uniform sampler2D LockFrame, Front;
+
+uniform float lod;
+
+#extension GL_ARB_shader_texture_lod : enable
 
 void main(void)
 {
-	vec2 st = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h);
-	vec4 front = texture2D(Front, st);
+	vec2 st = gl_FragCoord.xy / res;
 
-	gl_FragColor = front;
+	vec4 front = texture2D(Front, st);
+	vec4 front_avg = texture2DLod(Front, st, lod);
+	vec4 lock_frame = texture2DLod(LockFrame, st, lod);
+
+	vec4 lum = vec4(0.2125, 0.7154, 0.0721, 0.0);
+
+	float a = dot(front_avg, lum);
+	float l = dot(lock_frame, lum);
+
+	float new_gain = l / a;
+	front *= new_gain;
+
+	gl_FragColor = vec4(front.rgb, new_gain);
 }
